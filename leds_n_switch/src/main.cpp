64003cc511dd07761e7ae8631ae00467a1d2724a
lukaszkuczynski <kuczynskilukasz@gmail.com>
2020-11-08 20:07:35 +0100

    Switch-Led project modifications

--- leds_n_switch/src/main.cpp
+++ leds_n_switch/src/main.cpp
@@ -1,28 +1,32 @@
 #include <Arduino.h>
 
-int ledPin = 12; 
-
-int SWITCH_PIN = 14;
+#define LED_PIN 12
+#define SWITCH_PIN 14
  
+ICACHE_RAM_ATTR void detectsClick() {
+  Serial.println("click detected!");
+  digitalWrite(LED_PIN, HIGH);
+}
 
 void setup() {
   Serial.begin(115200);
-  pinMode(ledPin, OUTPUT); // ustawiamy pin jako wyjście
-  pinMode(SWITCH_PIN, INPUT);
+  pinMode(LED_PIN, OUTPUT); 
+  pinMode(SWITCH_PIN, INPUT_PULLUP);
+  attachInterrupt(digitalPinToInterrupt(SWITCH_PIN), detectsClick, RISING);
 }
- 
-// funkcja loop uruchamia się w nieskończonej pętli
+
+void instant_switch_to_led_loop() {
+  digitalWrite(LED_PIN, digitalRead(SWITCH_PIN));
+  delay(100);
+}
+
+void three_seconds_delay_led_loop() {
+  digitalWrite(LED_PIN, LOW);   
+  delay(3000);
+} 
+
 void loop() {
-  digitalWrite(ledPin, LOW);   // włączmy diodę, podajemy stan wysoki
-
-  int read = digitalRead(SWITCH_PIN);
-  
-
-  if (read == LOW) {
-    digitalWrite(ledPin, HIGH);
-  } else {
-    digitalWrite(ledPin, LOW);
-  }
-  
-  
+  instant_switch_to_led_loop();
+  // three_seconds_delay_led_loop();
 }
+
---
+++ openlight/readme.md
@@ -0,0 +1,1 @@
+https://randomnerdtutorials.com/esp8266-relay-module-ac-web-server/
