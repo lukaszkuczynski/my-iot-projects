1b0da452623ded512f8134a833a3dd952a2341eb
lukaszkuczynski <kuczynskilukasz@gmail.com>
2020-12-10 11:18:49 +0100

    Plant sensor improved

--- plant_life/src/main.cpp
+++ plant_life/src/main.cpp
@@ -1,30 +1,30 @@
 #include <Arduino.h>
+#include <mqtt.h>
+
+#define ONE_MINUTE 1e6 * 60
+
+// ADC_MODE(ADC_VDD);
 
 void setup()
 {
   Serial.begin(115200);
   pinMode(D0, WAKEUP_PULLUP);
   pinMode(D7, OUTPUT);
-}
-
-int loopval = 0;
-
-void loop()
-{
-
   digitalWrite(D7, HIGH);
-  delay(500);
+
+  connectWifi();
 
   int sensorValue = analogRead(A0);
-
-  // map it to the range of the PWM out
   int outputValue = map(sensorValue, 0, 1023, 0, 3300);
+  Serial.printf("ADC %d\n", outputValue);
+  send_mqtt_humidity(outputValue);
 
-  // print the readings in the Serial Monitor
-  Serial.printf("%d. %d\n", loopval, outputValue);
-  loopval++;
   digitalWrite(D7, LOW);
   delay(1000);
 
-  // ESP.deepSleep(5e6);
+  ESP.deepSleep(ONE_MINUTE * 5);
+}
+
+void loop()
+{
 }
