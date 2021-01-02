a27493ef8f7fcb518c79172ce6d07713aa0bf20c
lukaszkuczynski <kuczynskilukasz@gmail.com>
2021-01-02 13:54:28 +0100

    Sending telemetry to MQTT and accepting by Home Assistant

--- esp_projects/thermometer_oled_internal/thermometer_oled_internal.ino
+++ esp_projects/thermometer_oled_internal/thermometer_oled_internal.ino
@@ -1,6 +1,11 @@
 #include <Wire.h>
 #include <Adafruit_SSD1306.h>
 #include <Adafruit_BMP085.h>
+#include <ESP8266WiFi.h>
+#include "Adafruit_MQTT.h"
+#include "Adafruit_MQTT_Client.h"
+
+#include "secrets.h" // this file has definitions for 1)WIFI_SSID, 2) WIFI_PASSWORD, 3)MQTT_SERVER url
 
 #define SCREEN_WIDTH 128 // OLED display width, in pixels
 #define SCREEN_HEIGHT 32 // OLED display height, in pixels
@@ -14,6 +19,40 @@ Adafruit_BMP085 bmp;
 ADC_MODE(ADC_VDD); // measure internal voltage, please
 
 const int OLED_TOGGLE_PIN = 14;
+
+WiFiClient client;
+Adafruit_MQTT_Client mqtt(&client, MQTT_SERVER, 1883);
+Adafruit_MQTT_Publish temperature_publish = Adafruit_MQTT_Publish(&mqtt, "lukmqtt/internal_1/temperature");
+Adafruit_MQTT_Publish voltage_publish = Adafruit_MQTT_Publish(&mqtt, "lukmqtt/internal_1/voltage");
+
+const int SLEEP_TIME = 1e6 * 60 * 5;
+
+void connectWifi() {
+  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(F("."));
+  }
+  Serial.println(F(" WiFi connected."));
+}
+
+void sendTelemetry(float temp, int voltage) {
+  MQTT_connect();
+  int ret;
+  ret = temperature_publish.publish(temp);
+  if (!ret) {
+    Serial.println("Failed sending telemetry!!");
+  } else {
+    Serial.println("Telemetry sent.");
+  }
+  ret = voltage_publish.publish(voltage);
+  if (!ret) {
+    Serial.println("Failed sending telemetry!!");
+  } else {
+    Serial.println("Telemetry sent.");
+  }
+
+}
 
 void setup() {
   
@@ -44,24 +83,25 @@ void setup() {
     while (1);
   }
 
+  float temperature = bmp.readTemperature();
   Serial.print("Temperature = ");
-  Serial.print(bmp.readTemperature());
+  Serial.print(temperature);
   Serial.println(" *C");
-   
-  Serial.print("Pressure = ");
-  Serial.print(bmp.readPressure());
-  Serial.println(" Pa");
+  int internalVoltage = readInternalVoltage();
 
-  display_oled_temp(bmp.readTemperature());
+  displayTelemetryOLED(temperature, internalVoltage);
+
+  connectWifi();
+  sendTelemetry(temperature, internalVoltage);
   
   delay(5000);
   digitalWrite(OLED_TOGGLE_PIN, LOW); // Turn off the display
   Serial.println("Going sleep, bye");
 
-  ESP.deepSleep(5e6); 
+  ESP.deepSleep(SLEEP_TIME); 
 }
 
-void display_oled_temp(float temp) {
+void displayTelemetryOLED(float temp, int voltage) {
   display.clearDisplay();
   display.setCursor(0, 0);     // Start at top-left corner  
   display.setTextSize(3);      
@@ -70,14 +110,39 @@ void display_oled_temp(float temp) {
   display.println("C");
   // display battery state
   display.setTextSize(1);
-  int internalVoltage = ESP.getVcc();
-  Serial.printf("ADC %f\n", internalVoltage);
   display.print("battery: ");
-  display.print(internalVoltage);
+  display.print(voltage);
   display.print("mV");
   display.display();  
 }
 
+int readInternalVoltage() {
+  int internalVoltage = ESP.getVcc();
+  Serial.printf("ADC %f\n", internalVoltage);
+  return internalVoltage;
+}
+
+void MQTT_connect() {
+  int8_t ret;
+  // Stop if already connected.
+  if (mqtt.connected()) {
+    return;
+  }
+  Serial.print("Connecting to MQTT... ");
+  uint8_t retries = 3;
+  while ((ret = mqtt.connect()) != 0) { // connect will return 0 for connected
+       Serial.println(mqtt.connectErrorString(ret));
+       Serial.println("Retrying MQTT connection in 5 seconds...");
+       mqtt.disconnect();
+       delay(5000);  // wait 5 seconds
+       retries--;
+       if (retries == 0) {
+         // basically die and wait for WDT to reset me
+         while (1);
+       }
+  }
+  Serial.println("MQTT Connected!");
+}
 
 void loop() {
 }
