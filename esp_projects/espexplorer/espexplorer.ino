18e8a01610af917fa9881fbe09ef7165f301d805
lukaszkuczynski <kuczynskilukasz@gmail.com>
2021-03-29 22:19:47 +0200

    LED blinks when distance is measured

--- esp_projects/espexplorer/espexplorer.ino
+++ esp_projects/espexplorer/espexplorer.ino
@@ -1,5 +1,6 @@
 #define echoPin 5 // Echo Pin
 #define trigPin 2 // Trigger Pin
+const int LED_PIN = 4;
  
 long duration, distance; // Duration used to calculate distance
  
@@ -8,12 +9,13 @@ void setup()
   Serial.begin (9600);
   pinMode(trigPin, OUTPUT);
   pinMode(echoPin, INPUT);
+  pinMode(LED_PIN, OUTPUT);
 }
  
 void loop()
 {
+  digitalWrite(LED_PIN, HIGH);
   digitalWrite(trigPin, LOW);
-  
   delayMicroseconds(2);
   digitalWrite(trigPin, HIGH);
   delayMicroseconds(10);
@@ -23,5 +25,8 @@ void loop()
   distance = duration/58.2;
   Serial.println(distance);
   //Delay 50ms before next reading.
+  digitalWrite(LED_PIN, LOW);
   delay(500);
+
+  
 }
