0ef2f1e9d7ab7d67fe5e615366e496ba2dcf09ba
lukaszkuczynski <kuczynskilukasz@gmail.com>
2021-03-29 22:45:52 +0200

    blinking rate proportional to obstacle distance

--- esp_projects/espexplorer/espexplorer.ino
+++ esp_projects/espexplorer/espexplorer.ino
@@ -1,20 +1,26 @@
 #define echoPin 5 // Echo Pin
-#define trigPin 2 // Trigger Pin
-const int LED_PIN = 4;
+#define trigPin 4 // Trigger Pin
+const int LED_PIN = 2;
+
+enum LedStates{ON, OFF};
+LedStates ledState = ON;
+
+int led_delay;
  
 long duration, distance; // Duration used to calculate distance
+
+unsigned long chrono = millis();
  
 void setup()
 {
-  Serial.begin (9600);
+  led_delay = 1000;
+  Serial.begin(9600);
   pinMode(trigPin, OUTPUT);
   pinMode(echoPin, INPUT);
   pinMode(LED_PIN, OUTPUT);
 }
- 
-void loop()
-{
-  digitalWrite(LED_PIN, HIGH);
+
+float measure_distance() {
   digitalWrite(trigPin, LOW);
   delayMicroseconds(2);
   digitalWrite(trigPin, HIGH);
@@ -24,9 +30,36 @@ void loop()
   //Calculate the distance (in cm) based on the speed of sound.
   distance = duration/58.2;
   Serial.println(distance);
-  //Delay 50ms before next reading.
-  digitalWrite(LED_PIN, LOW);
-  delay(500);
+  return distance;  
+}
 
-  
+// https://github.com/j-bellavance/Tutorials/blob/master/State%20machines%20Tutorial/Part%201/BlinkWithoutDelay/BlinkWithoutDelay.ino
+void blinkMachine() {
+  switch (ledState) {
+    case OFF: {
+      if (millis() - chrono >= led_delay) {
+        chrono = millis();
+        digitalWrite(LED_PIN, HIGH);
+        ledState = ON;
+      }
+      break;
+    }
+    case ON: { 
+      if (millis() - chrono >= led_delay) {
+        chrono = millis();
+        digitalWrite(LED_PIN, LOW);
+        ledState = OFF;
+      }
+      break;
+    }
+  }
+}
+
+ 
+void loop()
+{
+  float distance = measure_distance();  
+  led_delay = 10 * distance;
+  blinkMachine();
+  delay(50);
 }
