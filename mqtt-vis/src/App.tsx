0255cda1b581b24f6e557dad496ab38027656340
lukaszkuczynski <kuczynskilukasz@gmail.com>
2021-05-19 07:11:21 +0200

    MQTT vis app simple with React

--- mqtt-vis/src/App.tsx
+++ mqtt-vis/src/App.tsx
@@ -1,23 +1,22 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import HookMqtt from './components/Hook/'
+
+
 
 function App() {
+
+
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h3>Ahead</h3>
+        <code>1.0</code>
+
+        <h3>Around</h3>
+        <code>...</code>
       </header>
     </div>
   );
