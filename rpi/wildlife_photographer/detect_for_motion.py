7cbafb33513987f39028ba30bc588ef49103552e
lukaszkuczynski <kuczynskilukasz@gmail.com>
2021-07-26 09:17:06 +0200

    Detects objects and draw boxes

--- rpi/wildlife_photographer/detect_for_motion.py
+++ rpi/wildlife_photographer/detect_for_motion.py
@@ -5,9 +5,11 @@ import re
 from PIL import Image
 from tflite_runtime.interpreter import Interpreter
 import numpy as np
+from object_detection_utils import draw_boxes
 
 PATTERN_TO_WATCH="/var/lib/motion/*.jpg"
 SAVED_MODEL_PATH = "./ssd_mobilenet.tflite"
+ALTERED_IMAGE_OUTPUT = "/home/pi/Pictures/wildlife/"
 
 interpreter = Interpreter(SAVED_MODEL_PATH)
 interpreter.allocate_tensors()
@@ -19,18 +21,21 @@ coco_labels = {}
 with open("./coco_labels.txt") as fread:
     lines = fread.read().split('\n')
     for line in lines: 
-        print(line)
         number = re.split("\s+", line)[0]
-        text = re.split("\s+", line)[1:]
+        text = ' '.join(re.split("\s+", line)[1:])
         coco_labels[int(number)] = text
 
-def interpret_results_coco(output_details):
+def interpret_results_coco(output_details, im_resized, filepath):
     boxes = interpreter.get_tensor(output_details[0]['index'])[0] 
-    print(boxes)
     classes = interpreter.get_tensor(output_details[1]['index'])[0] 
     scores = interpreter.get_tensor(output_details[2]['index'])[0]
-    class_names = [coco_labels[i] for i in classes.astype(int)]
+    class_names = [coco_labels[i] for i in classes.astype(int)]    
     print(list(zip(class_names,scores)))
+    altered_image_path = os.path.join(ALTERED_IMAGE_OUTPUT, os.path.basename(filepath))
+    altered_image_array = draw_boxes(im_resized, boxes, class_names, scores)
+    altered_image = Image.fromarray(altered_image_array)
+    altered_image.save(altered_image_path)
+    
 
 def handle_new_file(filepath):
     print("New file detected '%s'" % filepath)
@@ -42,7 +47,7 @@ def handle_new_file(filepath):
     input_data = np.array(im_resized, np.uint8)
     interpreter.set_tensor(input_details[0]['index'], [input_data])
     interpreter.invoke()
-    interpret_results_coco(output_details)
+    interpret_results_coco(output_details, im_resized, filepath)
     
 
 while True:
